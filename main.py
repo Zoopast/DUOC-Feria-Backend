cddcb2fcec83aa2b01f84d3c8a0b371c8db74559
Gabriel San Martin <gsandaur@hotmail.com>
2023-03-28 16:44:00 -0300

    feat: add more routes for tables

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from configs import config
-from routers import roles, users
+from routers import roles, users, bodegas, clientes, contratos, costos, embarque, empresas, ganancias, productoras, productos, seguros, ventas
 
 from db.client import get_cursor
 
 app = FastAPI()
 app.include_router(roles.router)
 app.include_router(users.router)
+app.include_router(bodegas.router)
+app.include_router(clientes.router)
+app.include_router(contratos.router)
+app.include_router(costos.router)
+app.include_router(embarque.router)
+app.include_router(empresas.router)
+app.include_router(ganancias.router)
+app.include_router(productoras.router)
+app.include_router(productos.router)
+app.include_router(seguros.router)
+app.include_router(ventas.router)
 
 
 @app.get("/")
