dbd06926165b5b162730970865b76de785bef011
Gabriel San Martin <gsandaur@hotmail.com>
2023-05-26 23:14:59 -0400

    feat: add finalizar subasta route

--- routers/subastas.py
+++ routers/subastas.py
@@ -36,80 +36,12 @@ async def obtener_subasta(id_subasta: int):
     connection.commit()
     return subasta_tuple_to_dict(result)
 
-@router.post("/")
-async def crear_subasta(subasta: Subasta):
-    nuevo_subasta = subasta.dict()
-    nuevo_subasta["estado"] = "enviado"
-    nuevo_subasta["fecha_inicio"] = datetime.strptime(nuevo_subasta["fecha_inicio"], "%d/%m/%Y").strftime("%d-%b-%Y")
-    nuevo_subasta["fecha_fin"] = datetime.strptime(nuevo_subasta["fecha_fin"], "%d/%m/%Y").strftime("%d-%b-%Y")
-    
-    del nuevo_subasta["id_subasta"]
-    del nuevo_subasta["productos"]
-
-
-    insert_query = """
-        INSERT INTO SUBASTAS (id_usuario, fecha_inicio, fecha_fin, estado, calidad)
-        VALUES (:id_usuario, :fecha_inicio, :fecha_fin, :estado, :calidad)
-        RETURNING id_subasta INTO :id_subasta
-    """
-
-    cursor, connection = get_cursor()
-    id_subasta = cursor.var(cx_Oracle.NUMBER)
-    cursor.execute(insert_query, id_subasta=id_subasta, **nuevo_subasta)
-
-    for producto in subasta.productos:
-        insert_query = """
-            INSERT INTO PRODUCTO_SUBASTA (id_subasta, nombre, cantidad)
-            VALUES (:id_subasta, :nombre, :cantidad)
-        """
-        producto["id_subasta"] = int(id_subasta.getvalue()[0])
-        cursor.execute(insert_query, producto)
-
-    connection.commit()
-
-    return {"message": "Subasta creado exitosamente"}
-
-@router.put("/{id_subasta}")
-async def actualizar_subasta(id_subasta: int, subasta: Subasta):
-    nuevo_subasta = subasta.dict()
-    nuevo_subasta["id_subasta"] = id_subasta
-    del nuevo_subasta["productos"]
-
+@router.put("/{id_subasta}/finalizar/")
+async def finalizar_subasta(id_subasta: int, id_transportista: int):
     update_query = """
-        UPDATE SUBASTAS
-        SET estado = :estado
-        WHERE id_subasta = :id_subasta
+        UPDATE SUBASTAS SET estado = 'finalizado', id_transportista = :id_transportista WHERE id_subasta = :id_subasta
     """
 
-    cursor, connection = get_cursor()
-    cursor.execute(update_query, id_subasta=id_subasta, estado=subasta.estado)
-
+    cursor.execute(update_query, id_subasta=id_subasta, id_transportista=id_transportista)
     connection.commit()
-
-    return {"message": "Subasta actualizado exitosamente"}
-
-@router.put("/{id_subasta}/estado")
-async def actualizar_estado_subasta(id_subasta: int, subasta: Subasta):
-    nuevo_subasta = subasta.dict()
-    nuevo_subasta["id_subasta"] = id_subasta
-    del nuevo_subasta["productos"]
-
-    update_query = """
-        UPDATE SUBASTAS
-        SET estado = :estado
-        WHERE id_subasta = :id_subasta
-    """
-
-    cursor, connection = get_cursor()
-    cursor.execute(update_query, id_subasta=id_subasta, estado=subasta.estado)
-
-    connection.commit()
-
-    return {"message": "Estado del subasta actualizado exitosamente"}
-
-@router.get("/requisitos_activos")
-async def obtener_requisitos_activos():
-    cursor.execute("SELECT * FROM SUBASTAS WHERE estado = 'activo'")
-    result = cursor.fetchall()
-    connection.commit()
-    return [subasta_tuple_to_dict(subasta) for subasta in result]
+    return {"message": "Subasta finalizada"}
