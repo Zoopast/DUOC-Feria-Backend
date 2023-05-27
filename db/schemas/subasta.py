52ee66d6027b654153c1eb4b54fd2ffa74b2d224
Gabriel San Martin <gsandaur@hotmail.com>
2023-05-26 23:31:16 -0400

    feat: add subastas routes

--- db/schemas/subasta.py
+++ db/schemas/subasta.py
@@ -1,9 +1,18 @@
 def subasta_tuple_to_dict(subasta: tuple):
     return {
         "id_subasta": subasta[0],
-        "id_usuario": subasta[1],
+        "id_requerimiento": subasta[1],
         "fecha_inicio": subasta[2],
         "fecha_fin": subasta[3],
         "estado": subasta[4],
         "id_transportista": subasta[5] if subasta[5] else None,
 		}
+
+def subasta_info_to_dict(subasta, requerimiento):
+    return {
+        "id_subasta": subasta[0],
+        "id_requerimiento": subasta[1],
+        "fecha_inicio": subasta[2],
+        "fecha_fin": subasta[3],
+        "estado": subasta[4],
+    }
--- routers/subastas.py
+++ routers/subastas.py
@@ -3,6 +3,7 @@ from db.client import get_cursor
 import cx_Oracle
 from db.models.subasta import Subasta
 from db.schemas.subasta import subasta_tuple_to_dict
+from db.schemas.requerimiento import requerimiento_tuple_to_dict
 from datetime import datetime
 
 router = APIRouter(
@@ -31,17 +32,49 @@ async def obtener_subastas():
 async def obtener_subasta(id_subasta: int):
     cursor.execute("SELECT * FROM SUBASTAS WHERE id_subasta = :id_subasta", id_subasta=id_subasta)
     result = cursor.fetchone()
+
+    subasta = subasta_tuple_to_dict(result)
     if not result:
         raise HTTPException(status_code=404, detail="Subasta no encontrado")
     connection.commit()
-    return subasta_tuple_to_dict(result)
+    
+    return subasta
+
+@router.get("/{id_subasta}/info/")
+async def obtener_subasta_info(id_subasta: int):
+    cursor.execute("SELECT * FROM SUBASTAS WHERE id_subasta = :id_subasta", id_subasta=id_subasta)
+    result = cursor.fetchone()
+    if not result:
+        raise HTTPException(status_code=404, detail="Subasta no encontrado")
+    
+    subasta = subasta_tuple_to_dict(result)
+    cursor.execute("SELECT * FROM REQUERIMIENTOS WHERE id_requerimiento = :id_requerimiento", id_requerimiento=subasta["id_requerimiento"])
+    result = cursor.fetchone()
+    
+    requerimiento = requerimiento_tuple_to_dict(result)
+    
+    connection.commit()
+    return {"subasta": subasta_tuple_to_dict(result), "requerimiento": requerimiento}
 
 @router.put("/{id_subasta}/finalizar/")
 async def finalizar_subasta(id_subasta: int, id_transportista: int):
     update_query = """
         UPDATE SUBASTAS SET estado = 'finalizado', id_transportista = :id_transportista WHERE id_subasta = :id_subasta
     """
-
     cursor.execute(update_query, id_subasta=id_subasta, id_transportista=id_transportista)
     connection.commit()
+    
+    get_requerimiento_query = """
+        SELECT id_requerimiento FROM SUBASTAS WHERE id_subasta = :id_subasta
+    """
+
+    cursor.execute(get_requerimiento_query, id_subasta=id_subasta)
+    result = cursor.fetchone()
+    id_requerimiento = result[0]
+
+    update_requerimiento_query = """
+        UPDATE REQUERIMIENTOS SET estado = 'finalizado' WHERE id_requerimiento = :id_requerimiento"""
+    
+    cursor.execute(update_requerimiento_query, id_requerimiento=id_requerimiento)
+
     return {"message": "Subasta finalizada"}
