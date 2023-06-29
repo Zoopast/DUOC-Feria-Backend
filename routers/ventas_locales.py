270ee8caf975e53efbc2d43e5a38099ca8eb667b
Gabriel San Martín <gsandaur@hotmail.com>
2023-06-29 14:53:27 -0400

    feat: add comprador to venta-local

--- routers/ventas_locales.py
+++ routers/ventas_locales.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from db.schemas.venta_local import venta_local_tuple_to_dict_schema
 from db.client import get_cursor
 from datetime import datetime, timedelta
+from utils.usuarios import get_current_user
 cursor, connection = get_cursor()
 router = APIRouter(
     prefix="/ventas-locales",
@@ -89,3 +90,31 @@ async def finalize_past_sales():
 				cursor.execute(finalize_past_sale_query, id_venta_local=venta_local[0])
 		connection.commit()
 		return {"message": "Ventas locales finalizadas"}
+
+@router.post('/{id_venta_local}/comprar/')
+async def comprar_venta_local(id_venta_local: int, user=Depends(get_current_user)):
+	try:
+		comprar_venta_local_query = """
+		UPDATE VENTAS_LOCALES SET estado = 'comprado', id_comprador=:id_usuario
+		WHERE id_venta_local = :id_venta_local
+		"""
+		cursor.execute(comprar_venta_local_query, id_venta_local=id_venta_local, id_usuario=user.id_usuario)
+		connection.commit()
+	except Exception as e:
+		print(e)
+		raise HTTPException(status_code=400, detail="Error comprando venta local")
+
+	return {"message": "Venta local comprada"}
+
+@router.post('/finalize-sale/{id_venta_local}')
+async def finalize_sale(id_venta_local: int):
+		try:
+			finalize_sale_query = """
+			UPDATE VENTAS_LOCALES SET estado = 'finalizado' WHERE id_venta_local = :id_venta_local
+			"""
+			cursor.execute(finalize_sale_query, id_venta_local=id_venta_local)
+			connection.commit()
+		except Exception as e:
+			raise HTTPException(status_code=404, detail="Venta local no encontrada")
+
+		return {"message": "Venta local finalizada"}
